198f2ac0cf15b0f1d405f64072eeb544669d2842
juick9207 <juick9207@163.com>
2014-11-06 14:32:44 +0900

    dd

---
+++ sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+    createCanvas(1500, 1000);
+}
+
+
+function draw() {
+   background(155);
+     
+   fill(255,255,0);
+   ellipse(750,500,500,500);
+
+
+
+
+   var duration = 10000;
+   var timing = (new Date()%duration)/duration;
+
+   fill(100,100,255);
+    ellipse(750 + Math.cos(timing*2*PI)*560,
+          500+Math.sin(timing*2*PI)*560,
+          250,
+          250);
+
+    fill(150,70,155);
+    ellipse(750 + Math.cos(timing*8*PI)*400,
+          500+Math.sin(timing*8*PI)*400,
+          170,
+          170);
+
+   fill(196,158,0);
+   ellipse(750 + Math.cos(timing*2*PI)*560 + Math.cos(timing*4*PI)*200,           // x좌표
+        500+Math.sin(timing*2*PI)*560 + Math.sin(timing*4*PI)*200,       // y좌표
+        50,                                     // width
+        50);                                        // height
+
+
+
+   fill(255,150,20);
+   ellipse(750 + Math.cos(timing*2*PI)*560 + Math.cos(timing*2*PI)*150,           // x좌표
+        500+Math.sin(timing*2*PI)*560 + Math.sin(timing*2*PI)*150,       // y좌표
+        30,                                     // width
+        30);    
+
+}
