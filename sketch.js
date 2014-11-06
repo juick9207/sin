8b359a92176b98543fd370cb6532744f92d813c3
juick9207 <juick9207@163.com>
2014-11-06 15:30:19 +0900

    a

--- sketch.js
+++ sketch.js
@@ -24,19 +24,19 @@ function draw() {
    fill(100,100,255);
     strokeWeight(2);
     ellipse(750 + Math.cos(timing*2*PI)*560,
-          500+Math.sin(timing*2*PI)*560,
+          500+Math.sin(timing*2*PI)*280,
           250,
           250);
 
     fill(150,70,155);
     ellipse(750 + Math.cos(timing*8*PI)*400,
-          500+Math.sin(timing*8*PI)*400,
+          500+Math.sin(timing*8*PI)*200,
           170,
           170);
 
    fill(196,158,0);
    ellipse(750 + Math.cos(timing*2*PI)*560 + Math.cos(timing*4*PI)*200,           // x좌표
-        500+Math.sin(timing*2*PI)*560 + Math.sin(timing*4*PI)*200,       // y좌표
+        500+Math.sin(timing*2*PI)*280 + Math.sin(timing*4*PI)*200,       // y좌표
         50,                                     // width
         50);                                        // height
 
@@ -44,7 +44,7 @@ function draw() {
 
    fill(255,150,20);
    ellipse(750 + Math.cos(timing*2*PI)*560 + Math.cos(timing*2*PI)*150,           // x좌표
-        500+Math.sin(timing*2*PI)*560 + Math.sin(timing*2*PI)*150,       // y좌표
+        500+Math.sin(timing*2*PI)*280 + Math.sin(timing*2*PI)*150,       // y좌표
         30,                                     // width
         30);    
 
